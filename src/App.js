b5da039faabb4df71552a0718c9e490ed41d1626
nasyr <hyrovn@gmail.com>
2023-04-05 15:31:44 +0600

    add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
+import { Route, Routes } from "react-router"; 
+import Layout from "./components/Layout/layout";
+import Nav from "./components/Nav/Nav" 
 
 function App() {
   return (
     <div className="App">
-    </div>
-  );
-}
 
-export default App;
+        <Layout> 
+          <Routes> 
+            <Route path="/" element={<h1>Home</h1>} /> 
+            <Route path="/about" element={<h1>About</h1>} /> 
+            <Route path="/contacts" element={<h1>Contacts</h1>} /> 
+            <Route path="/delivery" element={<h1>Delivery</h1>} /> 
+ 
+          </Routes> 
+        </Layout> 
+    </div> 
+  ); 
+} 
+ 
+export default App
+
