69270b998aa03e96f06cb95c9a2bd273d818e235
nasyr <hyrovn@gmail.com>
2023-04-05 15:44:23 +0600

    add Nav components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import { Route, Routes } from "react-router"; 
 import Layout from "./components/Layout/layout";
-import Nav from "./components/Nav/Nav" 
-
+import Nav from "./components/Nav/Nav";
 function App() {
   return (
     <div className="App">
 
         <Layout> 
           <Routes> 
-            <Route path="/" element={<h1>Home</h1>} /> 
+            <Route path="/" element={<Home />} /> 
             <Route path="/about" element={<h1>About</h1>} /> 
             <Route path="/contacts" element={<h1>Contacts</h1>} /> 
             <Route path="/delivery" element={<h1>Delivery</h1>} /> 
