0e8aafa26ed79863f5401d946d2f6eed977414b2
nasyr <hyrovn@gmail.com>
2023-04-10 15:43:15 +0600

    exchange categoryList and layout

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,43 +1,38 @@
+import { getDocs } from "firebase/firestore/lite";
 import { NavLink } from "react-router-dom";
 import { categories } from "../../firebase";
+import { useEffect, useState } from "react";
 
 export default function CategoryList() {
-    getDocs(categories).then (snapshot => {
-        const newCategoryList = [];
-        snapshot.docs.forEach(doc => {
-            const category = doc.data();
-            category.id = doc.id;
+  const [categoryList, setCategoryList] = useState([]);
 
-            newCategoryList(nevCategoryList);
-
-    });
-    
-
+  // получить документы из списка категорий в Firebase.
+  useEffect(() => {
+    getDocs(categories).then(snapshot => {
+      const newCategoryList = [];
+      snapshot.docs.forEach(doc => {
+        const category = doc.data(); // { name: "...", slug: "..." }
+        category.id = doc.id;
+  
+        newCategoryList.push(category);
+      })
+  
       setCategoryList(newCategoryList);
-    }); 
+    });
+  }, []);
 
+  const output = categoryList.map(category => (
+    <li key={category.id}>
+      <NavLink to={`/categories/${category.slug}`}>
+        {category.name}
+      </NavLink>
+    </li>
+  ));
 
-    // const categories = [
-    //     {id:  1, name: "In containers", slug: "In-containers" },
-    //     { id:  2, name: "In cones", slug: "In-cones" },
-    //     { id:  3, name: "Classic", slug: "classic" },
-    //     { id:  4, name: "Fruity", slug: "fruity" },
-    // ];
-
-    const output = categories.map((category) =>(
-        <li key={category.id}>
-            <NavLink to={"/categories/" + category.slug}>
-                {category.name}
-            </NavLink>
-        </li>
-
-    ));
-
-
-    return (
-        <div className="CategoryList">
-            <h3>Categories</h3>
-            <ul>{output}</ul>
-        </div>
-    );
-}    
+  return (
+    <div className="CategoryList">
+      <h3>Categories</h3>
+      <ul>{output}</ul>
+    </div>
+  );
+}
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,22 +1,20 @@
+import CategoryList from "../CategoryList/CategoryList";
+import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
-import"./Layout.css";
-import Logo from "../logo/Logo";
-
+import "./Layout.css";
 
 export default function Layout(props) {
-    return (
-        <div className="Layout">
-            <header>
-                <Logo />
-                <Nav/>
-            </header>
-            <aside>
-                <nav>NAV CATEGORIES</nav>
-            </aside>
-            <main>{props.children}</main>
-            <footer>
-                FOOTER
-            </footer>
-        </div>
-    );
+  return (
+    <div className="Layout">
+      <header>
+        <Logo />
+        <Nav />
+      </header>
+      <aside>
+        <CategoryList />
+      </aside>
+      <main>{props.children}</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
 }
