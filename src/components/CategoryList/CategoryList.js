d03af3701284dee22b354b757082a44392e88d12
nasyr <hyrovn@gmail.com>
2023-04-10 15:15:33 +0600

    exhange category

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,12 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { categories } from "../../firebase";
 
 export default function CategoryList() {
-    const categories = [
-        {id:  1, name: "In containers", slug: "In-containers" },
-        { id:  2, name: "In cones", slug: "In-cones" },
-        { id:  3, name: "Classic", slug: "classic" },
-        { id:  4, name: "Fruity", slug: "fruity" },
-    ];
+    getDocs(categories).then (snapshot => {
+        console.log(snapshot.docs);
+    });
+
+
+    // const categories = [
+    //     {id:  1, name: "In containers", slug: "In-containers" },
+    //     { id:  2, name: "In cones", slug: "In-cones" },
+    //     { id:  3, name: "Classic", slug: "classic" },
+    //     { id:  4, name: "Fruity", slug: "fruity" },
+    // ];
 
     const output = categories.map((category) =>(
         <li key={category.id}>
