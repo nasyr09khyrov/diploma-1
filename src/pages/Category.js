364f5244fbf28c428e99087fb7ec664cd0536161
nasyr <hyrovn@gmail.com>
2023-04-10 15:44:37 +0600

    exchange category.js

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,32 +1,27 @@
 import { useMatch } from "react-router-dom";
+import NotFound from "./NotFound";
 
 export default function Category() {
-    const { params } = useMatch("/categories/:slug");
+  const { params } = useMatch("/categories/:slug");
 
+  const categories = [
+    { id: 1, name: "In containers", slug: "in-containers" },
+    { id: 2, name: "In cones", slug: "in-cones" },
+    { id: 3, name: "Classic", slug: "classic" },
+    { id: 4, name: "Fruity", slug: "fruity" },
+  ];
 
+  const category = categories.find(
+    (category) => category.slug === params.slug
+  );
 
-    const categories = [
-        {id:  1, name: "In containers", slug: "In containers" },
-        { id:  2, name: "In cones", slug: "In-cones" },
-        { id:  3, name: "Classic", slug: "classic" },
-        { id:  4, name: "Fruity", slug: "fruity" },
-    ];
+  if (!category) {
+    return <NotFound />
+  }
 
-    const category = categories.find(
-        (category) => category.slug === params.slug
-    );
-
-
-    if (!category) {
-        return <NotFound />
-    }
-
-
-    return (
-        <div className="Category">
-            <h1>
-                {Category.name}
-            </h1>
-        </div>
-    );
+  return (
+    <div className="Category">
+      <h1>{category.name}</h1>
+    </div>
+  );
 }
