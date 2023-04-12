306e85844551f52655a3266d09cb1bc6058a80db
nasyr <hyrovn@gmail.com>
2023-04-12 15:16:20 +0600

    add cetegory name

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,12 +1,15 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
-import { useContext } from "react";
-import { AppContext } from "../App";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
 
-  const categories = useContext(AppContext);
+  const categories = [
+    { id: 1, name: "In containers", slug: "in-containers" },
+    { id: 2, name: "ski sticks", slug: "ski-sticks" },
+    { id: 3, name: "ski atomic", slug: "ski-atomic" },
+    { id: 4, name: "gloves leki", slug: "gloves-leki" },
+  ];
 
   const category = categories.find(
     (category) => category.slug === params.slug
