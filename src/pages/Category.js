3a24d56f9c6d753177c766a11215b562d8ebf04d
nasyr <hyrovn@gmail.com>
2023-04-12 15:17:56 +0600

    add appcontext

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,15 +1,12 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
 
-  const categories = [
-    { id: 1, name: "In containers", slug: "in-containers" },
-    { id: 2, name: "ski sticks", slug: "ski-sticks" },
-    { id: 3, name: "ski atomic", slug: "ski-atomic" },
-    { id: 4, name: "gloves leki", slug: "gloves-leki" },
-  ];
+  const { categories } = useContext(AppContext)
 
   const category = categories.find(
     (category) => category.slug === params.slug
