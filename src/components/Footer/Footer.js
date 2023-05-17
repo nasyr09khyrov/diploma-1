3c211074b4c2c5cb6c2a29ef41f5fef40c227818
nasyr <hyrovn@gmail.com>
2023-05-17 22:56:43 +0600

    added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="Footer__content">
+        <div className="Footer__column">
+          <h3 className="Footer__column-header">About Us</h3>
+          <ul className="Footer__column-list">
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Our Team</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+  );
+}
