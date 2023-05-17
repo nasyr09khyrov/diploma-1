a1baf63669d15fba3b15778b96a343efe26f412e
nasyr <hyrovn@gmail.com>
2023-05-17 15:10:41 +0600

    added js to Drawer js import

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,19 @@
+import Nav from "../Nav/Nav";
+import Logo from "../iu/Logo/Logo";
+import "./Drawer.css"
+
+function Drawer({ open, toggle }) {
+  const drawerClassNames = `Drawer ${open ? "open" : ""}`;
+
+  return (
+    <div className={drawerClassNames}>
+      <div onClick={toggle} className="backdrop"></div>
+      <div className="content">
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
