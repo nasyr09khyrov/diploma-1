21b18a043217b8cbec298572ac9473048b278917
nasyr <hyrovn@gmail.com>
2023-05-17 22:53:41 +0600

    import

--- src/components/NavToggle/NavToggle.js
+++ src/components/NavToggle/NavToggle.js
@@ -1,13 +1,1 @@
 import "./NavToggle.css";
-
-function NavToggle({ callback }) {
-  return (
-    <button onClick={callback} className="NavToggle">
-      <div className="line"></div>
-      <div className="line"></div>
-      <div className="line"></div>
-    </button>
-  );
-}
-
-export default NavToggle;
