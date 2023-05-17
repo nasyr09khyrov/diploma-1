70144d8d13be70777b951aabf785170f0e5f9ac8
nasyr <hyrovn@gmail.com>
2023-05-17 22:57:20 +0600

    added footer column

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,5 +12,13 @@ export default function Footer() {
             <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
+        <div className="Footer__column">
+          <h3 className="Footer__column-header">For Customers</h3>
+          <ul className="Footer__column-list">
+            <li><a href="#">Shipping &amp; Delivery</a></li>
+            <li><a href="#">Returns &amp; Exchanges</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+        </div>
   );
 }
