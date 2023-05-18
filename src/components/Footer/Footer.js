5edd65ec1f11524a3e49ff93e8db20e141b558c6
nasyr <hyrovn@gmail.com>
2023-05-19 01:11:06 +0600

    added Contact us in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,15 @@ export default function Footer() {
             <p className="footer-description">
             You will need ski equipment to hit the slopes, such as ski boots, bindings, skis, and ski poles. Don't worry, you can find everything you need on our website for ski equipment!
             </p>
-          </div>
+          </div> 
+          <div className="footer-section"> 
+            <h3 className="footer-title">Contact Us</h3> 
+            <ul className="footer-contact-list"> 
+              <li className="footer-contact-item">Phone: +996 700-549-530</li> 
+              <li className="footer-contact-item">Email: hyrovn@gmail.com</li> 
+              <li className="footer-contact-item">Address: 11, Zhambulskaya, Karakol City</li> 
+            </ul> 
+          </div> 
         </div>
       </div>
       <div className="Footer__bottom">
