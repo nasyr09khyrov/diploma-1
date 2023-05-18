9a8ba5023ed2c0fcb085cd691f24374c2edea22f
nasyr <hyrovn@gmail.com>
2023-05-19 01:53:33 +0600

    change a proble follow us

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,14 @@ import "./Footer.css"
 
 export default function Footer() {
   return (
-    <div className="Footer">
+    <div className="footer">
       <div className="container">
-        <div className=""footer-content>
-          <div className=""footer-section>
+        <div className="footer-content">
+          <div className="footer-section">
             <h3 className="footer-title">About Us</h3>
             <p className="footer-description">
-            You will need ski equipment to hit the slopes, such as ski boots, bindings, skis, and ski poles. Don't worry, you can find everything you need on our website for ski equipment!
+            You will need ski equipment to hit the slopes, such as ski boots, bindings, skis, and ski poles. Don't worry, you can find everything 
+            you need on our website for ski equipment!
             </p>
           </div> 
           <div className="footer-section"> 
@@ -42,7 +43,7 @@ export default function Footer() {
           </div>  
         </div>
       </div>
-      <div className="Footer__bottom">
+      <div className="Footer-bottom">
         <p>© 2023 YourOnlineStore. All rights reserved.</p>
       </div>
     </div>
