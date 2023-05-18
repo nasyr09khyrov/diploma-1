a98630f54f6004ab5a95658114240401f0c4378e
nasyr <hyrovn@gmail.com>
2023-05-19 05:06:42 +0600

    added style

--- src/components/HomeContent/HomeContent.js
+++ src/components/HomeContent/HomeContent.js
@@ -1,9 +1,14 @@
 import "./HomeContent.css";
+import img from "./../../assets/ski-photo.jpg";
+
 
 export default function HomeContent() {
     return (
         <div className="Home">
-            <img src="{img}" alt="" />
+            <div className="content">
+                <img src={img} alt="" className="photo"/>
+                <h3 className="text-content">This product will surprise you with its quality</h3>
+            </div>
         </div>
     )
 }
