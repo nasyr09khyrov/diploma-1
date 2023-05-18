519b80868ff660abd0a33b2112dbd8d89fd3ad94
nasyr <hyrovn@gmail.com>
2023-05-19 03:47:18 +0600

    added HomeContent for HomePage

---
+++ src/components/HomeContent/HomeContent.js
@@ -0,0 +1,10 @@
+import "./HomeContent.css";
+import img from "./../../assets/ski-photo.jpg";
+
+export default function HomeContent() {
+    return (
+        <div className="HomeContent">
+            
+        </div>
+    )
+}
