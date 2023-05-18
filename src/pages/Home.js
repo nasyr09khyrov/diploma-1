047b34b44755a6b6d677f6981ba245216a702722
nasyr <hyrovn@gmail.com>
2023-05-19 03:31:53 +0600

    deleted old photo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 export default function Home () {
     return (
         <div className="Home">
-            <h1>Home</h1>
-            <p>THis is my home page</p>
+            <div className="imag">
+                <img src="./"
+            </div>
         </div>
     );
 }
