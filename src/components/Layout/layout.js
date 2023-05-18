dda893b6083193271ca5fd7ba8fa57b0a01411cf
nasyr <hyrovn@gmail.com>
2023-05-18 13:33:45 +0600

    imported navtoggle drawer and footer

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -4,13 +4,27 @@ import Nav from "../Nav/Nav";
 import "./Layout.css";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
+import { useState } from "react";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
+import Footer from "../Footer/Footer";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
+
+
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
+
         <CartLink />
         <Auth />
       </header>
@@ -18,7 +32,9 @@ export default function Layout(props) {
         <CategoryList />
       </aside>
       <main>{props.children}</main>
-      <footer>FOOTER</footer>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
