f5280601e55698133ddf486b3d853461d954e41b
nasyr <hyrovn@gmail.com>
2023-05-19 12:52:27 +0600

    delete a photo

--- src/components/HomeContent/HomeContent.js
+++ src/components/HomeContent/HomeContent.js
@@ -6,7 +6,6 @@ export default function HomeContent() {
     return (
         <div className="Home">
             <div className="content">
-                <img src={img} alt="" className="photo"/>
                 <h3 className="text-content">This product will surprise you with its quality</h3>
             </div>
         </div>
