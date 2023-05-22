06ce44d907f8060dc85a47e20f0f6784e4e0f68b
nasyr <hyrovn@gmail.com>
2023-05-22 13:56:33 +0600

    filled AboutInfo

---
+++ src/components/AboutInfo/AboutInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AboutInfo.css';
+
+export default function AboutInfo() {
+  return (
+    <div className="AboutInfo">
+      <h2>All about our ski equipment store</h2>
+      <p>Welcome to our website of ski equipment here you will be surprised by the quality of products and a large assortment of ski equipment.</p>
+      <p>We are your reliable partner in the world of skiing. Our website offers a wide range of high-quality ski equipment that will help you reach new heights and enjoy unique moments on the slope</p>
+      <p>On our website you will find everything you need for a full-fledged skiing experience. We have a wide selection of skis, ski boots, mounts, helmets, glasses and other accessories from the world's leading manufacturers. We are proud to offer only high-quality products that meet the highest standards of safety and reliability.</p>
+      <p>Our experts carefully select each product to provide you with the best choice. We make sure that you get the maximum pleasure from skiing, and therefore we offer only those products that correspond to the latest trends in the industry and demonstrate high performance on the slopes.</p>
+      <p>We also have convenient delivery options so that you can receive your ordered equipment without unnecessary hassle. We value your time and strive to exceed your expectations in the field of speed and reliability of delivery.</p>
+      <p>Thank you for choosing our ski equipment website</p>
+    </div>
+  );
+}
