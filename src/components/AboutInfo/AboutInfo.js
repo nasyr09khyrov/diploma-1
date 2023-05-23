fff391274be0405528ff76c2ebb125db0f0205b4
nasyr <hyrovn@gmail.com>
2023-05-23 14:30:37 +0600

    delete one page

--- src/components/AboutInfo/AboutInfo.js
+++ src/components/AboutInfo/AboutInfo.js
@@ -9,7 +9,6 @@ export default function AboutInfo() {
       <p>We are your reliable partner in the world of skiing. Our website offers a wide range of high-quality ski equipment that will help you reach new heights and enjoy unique moments on the slope</p>
       <p>On our website you will find everything you need for a full-fledged skiing experience. We have a wide selection of skis, ski boots, mounts, helmets, glasses and other accessories from the world's leading manufacturers. We are proud to offer only high-quality products that meet the highest standards of safety and reliability.</p>
       <p>Our experts carefully select each product to provide you with the best choice. We make sure that you get the maximum pleasure from skiing, and therefore we offer only those products that correspond to the latest trends in the industry and demonstrate high performance on the slopes.</p>
-      <p>We also have convenient delivery options so that you can receive your ordered equipment without unnecessary hassle. We value your time and strive to exceed your expectations in the field of speed and reliability of delivery.</p>
       <p>Thank you for choosing our ski equipment website</p>
     </div>
   );
